c8805c1ffb6dbcec864b2e88b177a499f71f9bd6
Njal Karevoll <njal@karevoll.no>
2013-10-24 12:33:15 +0200

    depend on 0.90.3

--- project/Build.scala
+++ project/Build.scala
@@ -5,11 +5,11 @@ object Build extends Build {
   val foundOrganizationName = "Found AS"
   val foundOrgnizationPrefix = "no.found"
 
-  val elasticsearchVersion = "0.90.5"
+  val elasticsearchVersion = "0.90.3"
 
   val transportOrganization = foundOrgnizationPrefix + ".elasticsearch"
   val transportName = "elasticsearch-transport-module"
-  val transportVersion = "0.8.0-0905-SNAPSHOT"
+  val transportVersion = "0.8.0-0903-SNAPSHOT"
 
   var transportDependencies = Seq[ModuleID]()
 
